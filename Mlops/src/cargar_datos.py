6ab6bac41d27077727997b5a9f0b014621721155
friedjose <jose.villegaslora@gmail.com>
2025-11-02 13:35:35 -0500

    EDA and added clean table on GCP

--- Mlops/src/cargar_datos.py
+++ Mlops/src/cargar_datos.py
@@ -2,16 +2,19 @@ import json
 from google.cloud import bigquery
 import os
 
-# Load config
-config_path = os.path.join(os.path.dirname(__file__), "config.json")
-with open(config_path, 'r') as f:
-    config = json.load(f)
+def load_data():
+    # Load config
+    config_path = os.path.join(os.path.dirname(__file__), "config.json")
+    with open(config_path, 'r') as f:
+        config = json.load(f)
 
-project_id = config["proyecto"]  # Make sure you fill it in config.json
+    project_id = config["proyecto"]  # Fill this in config.json
+    client = bigquery.Client(project=project_id)
 
-client = bigquery.Client(project=project_id)
+    query = f"SELECT * FROM `{project_id}.proyecto_final_cdp.restaurantes_prediccion`"
+    df = client.query(query).to_dataframe()
+    return df
 
-query = f"SELECT * FROM `{project_id}.proyecto_final_cdp.restaurantes_prediccion`"
-df = client.query(query).to_dataframe()
-
-print(df.head())
+if __name__ == "__main__":
+    df = load_data()
+    print(df.head())
